81589761c52690fc7937458e47b265d769816761
tuan <tuanbosstrum@gmail.com>
2022-01-27 20:12:53 +0700

    bắt lỗi form đăng ký, đang làm hoàn thiện chức năng

---
+++ myshop_tutorial/public/js/form.js
@@ -0,0 +1,70 @@
+const loader = document.querySelector('.loader');
+// select input
+const submitBtn = document.querySelector('.submit-btn');
+const name = document.querySelector('#name');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const number = document.querySelector('#number');
+const tac = document.querySelector('#terms-and-cond');
+const notification = document.querySelector('#notification');
+
+submitBtn.addEventListener('click', () => {
+    if (name.value.length < 3) {
+        showAlert('name must be 3 letters long');
+    } else if (!email.value.length) {
+        showAlert('Enter your email');
+    } else if (password.value.length < 8) {
+        showAlert('password should be 8 letters long');
+    } else if (!number.value.length) {
+        showAlert('enter your phone number');
+    } else if (!Number(number.value) || number.value.length < 10) {
+        showAlert('invalid number, please enter a valid one');
+    } else if (!tac.checked) {
+        showAlert('you must agree to our terms and conditions');
+    } else {
+        //chạy
+        loader.style.display = 'block';
+        senData('/signup', {
+            name: name.value,
+            email: email.value,
+            password: password.value,
+            number: number.value,
+            tac: tac.checked,
+            notification: notification.checked,
+            seller: false
+        })
+    }
+})
+
+//tạo kiểu chức năng
+const senData = (path, data) => {
+    fetch(path, {
+            method: 'post',
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+            body: JSON.stringify(data)
+        }).then((res) => res.json())
+        .then(response => {
+            processData(response);
+        })
+}
+
+//kiểm tra dữ liệu đã có hay chưa
+const processData = (data) => {
+    loader.style.display = null;
+    if (data.alert) {
+        showAlert(data.alert);
+    } else if (data.name) {
+        console.log(data);
+    }
+}
+
+// lệnh cảnh báo
+const showAlert = (msg) => {
+    let alertBox = document.querySelector('.alert-box');
+    let alertMsg = document.querySelector('.alert-msg');
+    alertMsg.innerHTML = msg;
+    alertBox.classList.add('show');
+    setTimeout(() => {
+        alertBox.classList.remove('show');
+    }, 3000);
+}
--- myshop_tutorial/server.js
+++ myshop_tutorial/server.js
@@ -4,6 +4,14 @@ const admin = require('firebase-admin');
 const bcrypt = require('bcrypt');
 const path = require('path');
 
+//cài đặt tài khoản bên firebase
+let serviceAccount = require("./ecom-website-5415d-firebase-adminsdk-gjxgi-b1be4d9eea.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+let db = admin.firestore();
+
 //thư mục công cộng
 let staticPath = path.join(__dirname, 'public');
 
@@ -12,6 +20,7 @@ const app = express();
 
 //phương pháp tạo đường dẫn tĩnh
 app.use(express.static(staticPath));
+app.use(express.json());
 
 //tạo tuyến chủ để phản hồi
 app.get("/", (req, res) => {
@@ -21,6 +30,48 @@ app.get("/", (req, res) => {
 //đăng ký - đăng nhập
 app.get('/signup', (req, res) => {
     res.sendFile(path.join(staticPath, "signup.html"));
+})
+
+app.post('/signup', (req, res) => {
+    let { name, email, password, number, tac, notification } = req.body;
+
+    //bắt lỗi sự kiện validation
+    if (name.length < 3) {
+        return res.json({ 'alert': 'name must be letters long' });
+    } else if (!email.length) {
+        return res.json({ 'alert': 'Enter your email' });
+    } else if (password.length < 8) {
+        return res.json({ 'alert': 'password should be 8 letters long' });
+    } else if (!number.length) {
+        return res.json({ 'alert': 'enter your phone number' });
+    } else if (!Number(number) || number.length < 10) {
+        return res.json({ 'alert': 'invalid number, please enter a valid one' });
+    } else if (!tac) {
+        return res.json({ 'alert': 'you must agree to our terms and conditions' });
+    }
+
+    //đăng nhập user bằng firebase
+    db.collection('users').doc(email).get()
+        .then(user => {
+            if (user.exists) {
+                return res.json({ 'alert': 'email already exists' });
+            } else {
+                //mã hóa pass bằng thư viện bcrypt
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(password, salt, (err, hash) => {
+                        req.body.password = hash;
+                        db.collection('users').doc(email).set(req.body)
+                            .then(data => {
+                                res.json({
+                                    name: req.body.name,
+                                    email: req.body.email,
+                                    seller: req.body.seller,
+                                })
+                            })
+                    })
+                })
+            }
+        })
 })
 
 //bắt lỗi trang web 404 not found)
